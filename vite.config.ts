0760c0ec0a70c1dafc5965a920e34eb406c3e961
paulolima1212 <plima12121984@gmail.com>
2022-12-13 10:57:55 +0000

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bm-encomendas',
   plugins: [react()],
   server: {
     port: 3000,
